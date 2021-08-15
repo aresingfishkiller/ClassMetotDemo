d6feb98e28bbc2a78e862b7e3a59fbc88b12f81c
ttezc <ttezc@ARESING>
2021-08-15 20:54:04 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Isim + " " + "ekleme başarılı");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Isim + " " + "silme başarılı");
+        }
+        public void Listele(Musteri[] Musteriler)
+        {
+            foreach (Musteri musteri in Musteriler)
+            {
+                Console.WriteLine(musteri.Isim);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri musteri1 = new Musteri();
+            musteri1.Isim = "Tuna";
+            musteri1.SoyIsim = "Tezcan";
+            musteri1.Yas = 28;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Isim = "Uğur";
+            musteri2.SoyIsim = "Çınar";
+            musteri2.Yas = 27;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Isim = "Buğra";
+            musteri3.SoyIsim = "Çuhacı";
+            musteri3.Yas = 26;
+
+            Musteri[] Musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri3);
+            musteriManager.Listele(Musteriler);
+        }
+    }
+}
